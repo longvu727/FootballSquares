e1c05e7b72eb82078ceabef2150e031b8d5384b0
Long Vu <longvu727@yahoo.com>
2024-07-15 16:12:36 -0700

    add usermicroservices to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ git clone https://github.com/longvu727/FootballSquaresLibs
 git clone git@github.com:longvu727/SquareMicroservices.git Microservices/SquareMicroservices/
 git clone git@github.com:longvu727/GameMicroservices.git Microservices/GameMicroservices/
 git clone git@github.com:longvu727/FootballSquareGameMicroservices.git Microservices/FootballSquareGameMicroservices/
+git clone git@github.com:longvu727/UserMicroservices.git Microservices/UserMicroservices/
 
 git clone git@github.com:longvu727/FootballSquarePages.git FootballSquarePages
